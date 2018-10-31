acff933dc1219eae248355e655531dcd700b627d
Alberto Diaz Martin <alberto.diaz@encamina.com>
2018-10-31 16:04:59 +0100

    Application Insights

--- Generic-LUIS/Rodolfo/Rodolfo.LUIS.Alexa/Program.cs
+++ Generic-LUIS/Rodolfo/Rodolfo.LUIS.Alexa/Program.cs
@@ -19,6 +19,7 @@ namespace Rodolfo.LUIS.Alexa
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
